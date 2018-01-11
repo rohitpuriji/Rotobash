68263ebc0f0f7c668a58bb9d02b6f4d1b2de973c
tarunjoshi-evontech <tarunjoshi@evontech.com>
2018-01-11 18:16:03 +0530

    Validation check on reset password

--- app/src/main/java/com/tech/rotobash/Validations/FieldValidations.java
+++ app/src/main/java/com/tech/rotobash/Validations/FieldValidations.java
@@ -5,7 +5,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
-import com.tech.rotobash.activites.AuthenticationActivity;
 import com.tech.rotobash.databinding.ActivityLoginBinding;
 import com.tech.rotobash.databinding.LayoutResetPasswordBinding;
 import com.tech.rotobash.utils.AppUtils;
@@ -23,105 +22,112 @@ import static com.tech.rotobash.utils.AppConstant.sPasswordMismatch;
 import static com.tech.rotobash.utils.AppConstant.sReEnterPassword;
 
 /**
- @Module class/module		:	FieldValidations
- @Author Name			    :	Rohit Puri
- @Date					    :	Jan 8 , 2018
- @Purpose				    :	It contains all validation methods using Binding
+ * @Module class/module		:	FieldValidations
+ * @Author Name                :	Rohit Puri
+ * @Date                     :	Jan 8 , 2018
+ * @Purpose                 :	It contains all validation methods using Binding
  */
 public class FieldValidations {
 
-    public static boolean doLoginValidation(ActivityLoginBinding mBinding){
-        if (TextUtils.isEmpty(mBinding.etRegEmail.getText().toString())){
+    public static boolean doLoginValidation(ActivityLoginBinding mBinding) {
+        if (TextUtils.isEmpty(mBinding.etRegEmail.getText().toString())) {
             mBinding.etRegEmail.requestFocus();
             mBinding.tvErrorEmail.setVisibility(View.VISIBLE);
             mBinding.tvErrorEmail.setText(sEnterEmail);
             return false;
-        }else if (!AppUtils.isValidEmail(mBinding.etRegEmail.getText().toString())){
+        } else if (!AppUtils.isValidEmail(mBinding.etRegEmail.getText().toString())) {
             mBinding.etRegEmail.requestFocus();
             mBinding.tvErrorEmail.setVisibility(View.VISIBLE);
             mBinding.tvErrorEmail.setText(sEnterValidEmail);
             return false;
-        }else if (TextUtils.isEmpty(mBinding.etRegPassword.getText().toString())){
+        } else if (TextUtils.isEmpty(mBinding.etRegPassword.getText().toString())) {
             mBinding.etRegPassword.requestFocus();
             mBinding.tvErrorPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorPassword.setText(sEnterPassword);
             return false;
-        }else if (mBinding.etRegPassword.getText().toString().length() < 8){
+        } else if (mBinding.etRegPassword.getText().toString().length() < 8) {
             mBinding.etRegPassword.requestFocus();
             mBinding.tvErrorPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorPassword.setText(sEnterValidPassword);
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
 
-    public static boolean doRegValidation(AppCompatActivity appCompatActivity,ActivityLoginBinding mBinding){
-        if (TextUtils.isEmpty(mBinding.etName.getText().toString())){
+    public static boolean doRegValidation(AppCompatActivity appCompatActivity, ActivityLoginBinding mBinding) {
+        if (TextUtils.isEmpty(mBinding.etName.getText().toString())) {
             mBinding.etName.requestFocus();
             mBinding.tvErrorName.setVisibility(View.VISIBLE);
             mBinding.tvErrorName.setText(sEnterName);
             return false;
-        }else if (TextUtils.isEmpty(mBinding.etRegEmail.getText().toString())){
+        } else if (TextUtils.isEmpty(mBinding.etRegEmail.getText().toString())) {
             mBinding.etRegEmail.requestFocus();
             mBinding.tvErrorEmail.setVisibility(View.VISIBLE);
             mBinding.tvErrorEmail.setText(sEnterEmail);
             return false;
-        }else if (!AppUtils.isValidEmail(mBinding.etRegEmail.getText().toString())){
+        } else if (!AppUtils.isValidEmail(mBinding.etRegEmail.getText().toString())) {
             mBinding.etRegEmail.requestFocus();
             mBinding.tvErrorEmail.setVisibility(View.VISIBLE);
             mBinding.tvErrorEmail.setText(sEnterValidEmail);
             return false;
-        }else if (TextUtils.isEmpty(mBinding.etRegPassword.getText().toString())){
+        } else if (TextUtils.isEmpty(mBinding.etRegPassword.getText().toString())) {
             mBinding.etRegPassword.requestFocus();
             mBinding.tvErrorPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorPassword.setText(sEnterPassword);
             return false;
-        }else if (mBinding.etRegPassword.getText().toString().length() < 8){
+        } else if (mBinding.etRegPassword.getText().toString().length() < 8) {
             mBinding.etRegPassword.requestFocus();
             mBinding.tvErrorPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorPassword.setText(sEnterValidPassword);
             return false;
-        }else if(!AppUtils.validate(mBinding.etRegPassword.getText().toString())){
+        } else if (!AppUtils.validate(mBinding.etRegPassword.getText().toString())) {
             mBinding.etRegPassword.requestFocus();
             mBinding.tvErrorPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorPassword.setText(ePasswordReq);
             return false;
-        }else if (TextUtils.isEmpty(mBinding.etConfirmPass.getText().toString())){
+        } else if (TextUtils.isEmpty(mBinding.etConfirmPass.getText().toString())) {
             mBinding.etConfirmPass.requestFocus();
             mBinding.tvErrorConPassword.setVisibility(View.VISIBLE);
             mBinding.tvErrorConPassword.setText(sReEnterPassword);
             return false;
-        }else if (!mBinding.etConfirmPass.getText().toString().equals(mBinding.etRegPassword.getText().toString())){
-            Toast.makeText(appCompatActivity,sPasswordMismatch,Toast.LENGTH_LONG).show();
+        } else if (!mBinding.etConfirmPass.getText().toString().equals(mBinding.etRegPassword.getText().toString())) {
+            Toast.makeText(appCompatActivity, sPasswordMismatch, Toast.LENGTH_LONG).show();
             return false;
-        }else if (!mBinding.chkTnC.isChecked()){
-            Toast.makeText(appCompatActivity,sAgreeTnc,Toast.LENGTH_LONG).show();
+        } else if (!mBinding.chkTnC.isChecked()) {
+            Toast.makeText(appCompatActivity, sAgreeTnc, Toast.LENGTH_LONG).show();
             return false;
-        }else {
+        } else {
             return true;
         }
     }
 
     public static boolean doResetValidation(LayoutResetPasswordBinding mBinding) {
-        if (TextUtils.isEmpty(mBinding.etOldPass.getText().toString())){
+        if (TextUtils.isEmpty(mBinding.etOldPass.getText().toString())) {
             mBinding.etOldPass.requestFocus();
-            mBinding.etOldPass.setError(sEnterOldPassword);
+            mBinding.tvErrorOldPassword.setVisibility(View.VISIBLE);
+            mBinding.tvErrorOldPassword.setText(sEnterOldPassword);
+            //  mBinding.etOldPass.setError(sEnterOldPassword);
             return false;
-        }else if (TextUtils.isEmpty(mBinding.etNewPass.getText().toString())){
+        } else if (TextUtils.isEmpty(mBinding.etNewPass.getText().toString())) {
             mBinding.etNewPass.requestFocus();
-            mBinding.etNewPass.setError(sEnterNewPassword);
+            mBinding.tvErrorNewPassword.setVisibility(View.VISIBLE);
+            mBinding.tvErrorNewPassword.setText(sEnterNewPassword);
+            //  mBinding.etNewPass.setError(sEnterNewPassword);
             return false;
-        }else if (mBinding.etNewPass.getText().toString().length() < 8){
+        } else if (mBinding.etNewPass.getText().toString().length() < 8) {
             mBinding.etNewPass.requestFocus();
-            mBinding.etNewPass.setError(sEnterValidPassword);
+            mBinding.tvErrorNewPassword.setVisibility(View.VISIBLE);
+            mBinding.tvErrorNewPassword.setText(sEnterValidPassword);
+            //  mBinding.etNewPass.setError(sEnterValidPassword);
             return false;
-        }else if(!AppUtils.validate(mBinding.etNewPass.getText().toString())){
+        } else if (!AppUtils.validate(mBinding.etNewPass.getText().toString())) {
             mBinding.etNewPass.requestFocus();
-            mBinding.etNewPass.setError(ePasswordReq);
+            mBinding.tvErrorNewPassword.setVisibility(View.VISIBLE);
+            mBinding.tvErrorNewPassword.setText(ePasswordReq);
+            //     mBinding.etNewPass.setError(ePasswordReq);
             return false;
-        }else{
+        } else {
             return true;
         }
     }
--- app/src/main/java/com/tech/rotobash/activites/SidemenuActivity.java
+++ app/src/main/java/com/tech/rotobash/activites/SidemenuActivity.java
@@ -8,21 +8,17 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.tech.rotobash.R;
 import com.tech.rotobash.ViewModel.UserViewModel;
 import com.tech.rotobash.databinding.ActivitySidemenuBinding;
-import com.tech.rotobash.databinding.LayoutForgetPasswordBinding;
 import com.tech.rotobash.databinding.LayoutResetPasswordBinding;
 import com.tech.rotobash.model.UserResponse;
 import com.tech.rotobash.utils.AppConstant;
@@ -34,16 +30,14 @@ import java.util.concurrent.TimeUnit;
 
 import static com.tech.rotobash.Validations.FieldValidations.doResetValidation;
 import static com.tech.rotobash.utils.AppConstant.ePasswordReq;
-import static com.tech.rotobash.utils.AppConstant.sEnterNewPassword;
-import static com.tech.rotobash.utils.AppConstant.sEnterOldPassword;
 import static com.tech.rotobash.utils.AppConstant.sEnterValidPassword;
 import static com.tech.rotobash.utils.AppConstant.sPleaseWait;
 
 /**
- @Module Name/Class		:	SidemenuActivity
- @Author Name			:	Rohit Puri
- @Date					:	Jan 8rd , 2018
- @Purpose				:	This class contains both sidemenu screen and main matches contents
+ * @Module Name/Class		:	SidemenuActivity
+ * @Author Name            :	Rohit Puri
+ * @Date :	Jan 8rd , 2018
+ * @Purpose :	This class contains both sidemenu screen and main matches contents
  */
 public class SidemenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -61,7 +55,7 @@ public class SidemenuActivity extends AppCompatActivity
         mBinding.included.imgMenu.setOnClickListener(view -> {
             if (mBinding.drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
                 mBinding.drawerLayout.closeDrawer(Gravity.RIGHT);
-            }else{
+            } else {
                 mBinding.drawerLayout.openDrawer(Gravity.RIGHT);
             }
         });
@@ -79,12 +73,12 @@ public class SidemenuActivity extends AppCompatActivity
     }
 
     /**
-     @Module Name/Class		:	initData
-     @Author Name			:	Rohit Puri
-     @Date					:	Jan 8rd , 2018
-     @Purpose				:	This method initialize the basic functionality
+     * @Module Name/Class		:	initData
+     * @Author Name            :	Rohit Puri
+     * @Date :	Jan 8rd , 2018
+     * @Purpose :	This method initialize the basic functionality
      */
-    private void initData(){
+    private void initData() {
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_sidemenu);
         mUserResponse = getIntent().getExtras().getParcelable("UserResponse");
@@ -104,18 +98,18 @@ public class SidemenuActivity extends AppCompatActivity
 
         if (Network.isAvailable(SidemenuActivity.this)) {
             loadCurrentMatches();
-        }else {
-            Toast.makeText(SidemenuActivity.this, AppConstant.sNoInternet,Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(SidemenuActivity.this, AppConstant.sNoInternet, Toast.LENGTH_LONG).show();
         }
     }
 
     /**
-     @Module Name/Class		:	loadCurrentMatches
-     @Author Name			:	Rohit Puri
-     @Date					:	Jan 11th , 2018
-     @Purpose				:	This method loads the current matches from api
+     * @Module Name/Class		:	loadCurrentMatches
+     * @Author Name            :	Rohit Puri
+     * @Date :	Jan 11th , 2018
+     * @Purpose :	This method loads the current matches from api
      */
-    private void loadCurrentMatches(){
+    private void loadCurrentMatches() {
 
     }
 
@@ -159,8 +153,8 @@ public class SidemenuActivity extends AppCompatActivity
         } else if (id == R.id.nav_reset) {
             if (Network.isAvailable(SidemenuActivity.this)) {
                 showResetDialog();
-            }else {
-                Toast.makeText(SidemenuActivity.this, AppConstant.sNoInternet,Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(SidemenuActivity.this, AppConstant.sNoInternet, Toast.LENGTH_LONG).show();
             }
 
         } else if (id == R.id.nav_share) {
@@ -174,26 +168,28 @@ public class SidemenuActivity extends AppCompatActivity
     }
 
 
-    private void moveScreen(){
+    private void moveScreen() {
         Intent i = new Intent(SidemenuActivity.this, AuthenticationActivity.class);
         startActivity(i);
         finish();
     }
 
     /**
-     @Module Name/Class		:	showResetDialog
-     @Author Name			:	Rohit Puri
-     @Date					:	Jan 11th , 2018
-     @Purpose				:	This method shows the reset dialog
+     * @Module Name/Class		:	showResetDialog
+     * @Author Name            :	Rohit Puri
+     * @Date :	Jan 11th , 2018
+     * @Purpose :	This method shows the reset dialog
      */
-    private void showResetDialog(){
-        LayoutResetPasswordBinding mBinding  = DataBindingUtil
+    private void showResetDialog() {
+        LayoutResetPasswordBinding mBinding = DataBindingUtil
                 .inflate(LayoutInflater.from(SidemenuActivity.this), R.layout.layout_reset_password, null, false);
 
         final Dialog openDialog = new Dialog(SidemenuActivity.this);
         openDialog.setContentView(mBinding.getRoot());
         openDialog.setCancelable(true);
 
+        mBinding.tvErrorOldPassword.setVisibility(View.GONE);
+        mBinding.tvErrorNewPassword.setVisibility(View.GONE);
         AppUtils.showSoftKeyboard(openDialog);
 
         mBinding.btnCancel.setOnClickListener(v -> openDialog.dismiss());
@@ -202,20 +198,33 @@ public class SidemenuActivity extends AppCompatActivity
         RxTextView.textChanges(mBinding.etNewPass)
                 .debounce(delay, TimeUnit.MILLISECONDS)
                 .subscribe(textChanged -> runOnUiThread(() -> {
-                    if(mBinding.etNewPass.getText().toString().length() > 0) {
+                    if (mBinding.etNewPass.getText().toString().length() > 0) {
                         if (mBinding.etNewPass.getText().toString().length() < 8) {
                             mBinding.etNewPass.requestFocus();
-                            mBinding.etNewPass.setError(sEnterValidPassword);
+                            mBinding.tvErrorNewPassword.setVisibility(View.VISIBLE);
+                            mBinding.tvErrorNewPassword.setText(sEnterValidPassword);
+                            // mBinding.etNewPass.setError(sEnterValidPassword);
+
                         } else if (!AppUtils.validate(mBinding.etNewPass.getText().toString())) {
                             mBinding.etNewPass.requestFocus();
-                            mBinding.etNewPass.setError(ePasswordReq);
+                            mBinding.tvErrorNewPassword.setVisibility(View.VISIBLE);
+                            mBinding.tvErrorNewPassword.setText(ePasswordReq);
+                            // mBinding.etNewPass.setError(ePasswordReq);
                         }
                     }
                 }));
 
+        RxTextView.textChanges(mBinding.etOldPass)
+                .subscribe(charSequence ->
+                        mBinding.tvErrorOldPassword.setVisibility(View.GONE));
+
+        RxTextView.textChanges(mBinding.etNewPass)
+                .subscribe(charSequence ->
+                        mBinding.tvErrorNewPassword.setVisibility(View.GONE));
+
         mBinding.btnSubmit.setOnClickListener(view -> {
-            if (doResetValidation(mBinding)){
-                doResetPassword(openDialog,mBinding.etOldPass.getText().toString(),mBinding.etNewPass.getText().toString());
+            if (doResetValidation(mBinding)) {
+                doResetPassword(openDialog, mBinding.etOldPass.getText().toString(), mBinding.etNewPass.getText().toString());
             }
         });
 
@@ -223,20 +232,20 @@ public class SidemenuActivity extends AppCompatActivity
     }
 
     /**
-     @Module Name/Class		:	doResetPassword
-     @Author Name			:	Rohit Puri
-     @Date					:	Jan 11th , 2018
-     @Purpose				:	This method observe the response coming from ViewModel class in form of LiveData
+     * @Module Name/Class		:	doResetPassword
+     * @Author Name            :	Rohit Puri
+     * @Date :	Jan 11th , 2018
+     * @Purpose :	This method observe the response coming from ViewModel class in form of LiveData
      */
-    private void doResetPassword(Dialog aDialog,String aOldPass, String aNewPass){
+    private void doResetPassword(Dialog aDialog, String aOldPass, String aNewPass) {
 
         UserViewModel mSignupViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
 
-        mSignupViewModel.getResetPassword(progressDoalog,mUserResponse,aOldPass,aNewPass)
-                .observe(this,userResponse ->{
-                    Toast.makeText(SidemenuActivity.this,userResponse.getMessage(),Toast.LENGTH_LONG).show();
+        mSignupViewModel.getResetPassword(progressDoalog, mUserResponse, aOldPass, aNewPass)
+                .observe(this, userResponse -> {
+                    Toast.makeText(SidemenuActivity.this, userResponse.getMessage(), Toast.LENGTH_LONG).show();
                     AppUtils.hideSoftKeyboard(SidemenuActivity.this);
-                    if (userResponse.getStatus().equalsIgnoreCase("success")){
+                    if (userResponse.getStatus().equalsIgnoreCase("success")) {
                         aDialog.dismiss();
                     }
                 });
